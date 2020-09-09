185658206ae5f45becc31f56a160929ca0561cee
tomasz <tomasz.kiapsnia@gmail.com>
2020-09-09 23:41:03 +0200

    pull or clone repo

---
+++ repository/repository.py
@@ -0,0 +1,15 @@
+import git
+from git import Repo
+import os.path
+
+class Repos:
+    def __init__(self, git_url, repo_dir):
+        if os.path.isdir(repo_dir + '\\.git'):
+            g = git.cmd.Git(repo_dir)
+            g.pull()
+        else:
+            Repo.clone_from(git_url, repo_dir)
+
+
+if __name__ == '__main__':
+    r = Repos('git@github.com:TomaszKiapsnia/dqtest.git', 'F:\\test')
